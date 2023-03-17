2d6d405482d9399ed3a569613876214e591bfcb9
扫地僧 <jasonwang@victoryfintech.com.hk>
2023-03-17 14:15:13 +0800

    Update code.go

--- e/code.go
+++ e/code.go
@@ -7,13 +7,6 @@ const (
 	Unauthorized  = 401 // Unauthorized 请求要求身份验证
 	Forbidden     = 403 // Forbidden The client does not have access rights to the content
 	NotFound      = 404 // NotFound : The server can not find the requested resource
-
-	UserDisagreement      = 1000
-	UserDuplicateUsername = 1001
-	UserFreeze            = 1002
-	UserAuthFailed        = 1003
-
-	AccountNotRecord = 1010
 )
 
 var Err = New(ERROR)
@@ -25,11 +18,3 @@ var ErrUnauthorized = New(Unauthorized)
 var ErrForbidden = New(Forbidden)
 
 var ErrNotFound = New(NotFound)
-
-var ErrUserDisagreement = New(UserDisagreement)
-
-var ErrUserDuplicateUsername = New(UserDuplicateUsername)
-
-var ErrUserFreeze = New(UserFreeze)
-
-var ErrUserAuthFailed = New(UserAuthFailed)
